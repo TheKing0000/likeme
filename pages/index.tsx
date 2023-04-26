25b128ef931528835021bf8fd43c578369d1dfba
TheKing0000 <simon.kovesdi2@gmail.com>
2023-04-26 18:54:16 +0200

    typing effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/components/navigation/NavBar";
 import { useTheme } from "next-themes";
+import Typewriter from "typewriter-effect";
 export default function Home() {
   const { theme, setTheme } = useTheme();
 
@@ -9,6 +10,15 @@ export default function Home() {
       <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
         Change Theme
       </button>
+      <h1 className="uppercase mt-5">
+        <Typewriter
+          options={{
+            strings: ["Hello", "World"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </h1>
       <div className="h-screen"></div>
     </main>
   );
